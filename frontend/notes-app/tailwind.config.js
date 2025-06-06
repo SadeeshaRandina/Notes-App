a1b44d2a9c844713de8def2ece28498bdca674f9
SadeeshaRandina <sadee.randina25@gmail.com>
2025-06-07 01:33:17 +0530

    feat: add Navbar component and integrate it into the Login page

--- frontend/notes-app/tailwind.config.js
+++ frontend/notes-app/tailwind.config.js
@@ -1,8 +1,16 @@
-/** @type {import( 'tailwindcss' ) .Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
-    content: [],
-    theme: {
-        extend: {}
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#2B85FF",
+        secondary: "#EF863E",
+      },
     },
-    plugins: []
-}
+  },
+  plugins: [],
+};
