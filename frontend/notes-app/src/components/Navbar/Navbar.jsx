8076d59b5b0507f34c285058d368fad696c79f15
SadeeshaRandina <sadee.randina25@gmail.com>
2025-06-08 00:37:56 +0530

    feat: implement SearchBar component and integrate it into Navbar; add search functionality with state management

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import React, { use } from 'react'
 import ProfileInfo from '../../components/Cards/ProfileInfo'
 import { useNavigate } from 'react-router-dom';
+import SearchBar from '../../components/SearchBar/SearchBar'
+import { useState } from 'react';
 
 const Navbar = () => {
+
+  const [searchQuery, setSearchQuery] = useState("");
 
   const Navigate = useNavigate();
 
@@ -10,9 +14,26 @@ const Navbar = () => {
     Navigate("/login");
   };
 
+  const handleSearch = () => {
+
+  };
+
+  const onClearSearch = () => {
+    setSearchQuery("");
+  };
+
   return (
     <div className="bg-white flex items-center justify-between p-6 py-2 drop-shadow">
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+
+        <SearchBar 
+          value={searchQuery}
+          onChange={({target}) => {
+            setSearchQuery(target.value);
+          }}
+          handleSearch={handleSearch}
+          onClearSearch={onClearSearch}
+        />
 
         <ProfileInfo onLogout={onLogout}/>
     </div>
