548db179e66816cece88d62fb28eadb881adb304
SadeeshaRandina <sadee.randina25@gmail.com>
2025-06-08 00:01:06 +0530

    feat: add ProfileInfo component to display user initials and logout functionality; integrate into Navbar and Home pages

--- frontend/notes-app/src/components/Navbar/Navbar.jsx
+++ frontend/notes-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,20 @@
-import React from 'react'
+import React, { use } from 'react'
+import ProfileInfo from '../../components/Cards/ProfileInfo'
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
+
+  const Navigate = useNavigate();
+
+  const onLogout = () => {
+    Navigate("/login");
+  };
+
   return (
     <div className="bg-white flex items-center justify-between p-6 py-2 drop-shadow">
         <h2 className="text-xl font-medium text-black py-2">Notes</h2>
+
+        <ProfileInfo onLogout={onLogout}/>
     </div>
   )
 }
