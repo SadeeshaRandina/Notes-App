6b604c243d5db21733e7275013c0b0b42c2f55e3
SadeeshaRandina <sadee.randina25@gmail.com>
2025-06-11 00:25:27 +0530

    refactor: reorganize imports and improve error handling in create-account endpoint

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config(); 
-const express = require('express');
-const cors = require('cors');
-const app = express();
 const config = require('./config');
 const mongoose = require('mongoose');
 
 mongoose.connect(config.connectionString);
+
+const User = require('./models/user.model');
+
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
 
 const jwt = require('jsonwebtoken');
 const { authenticateToken } = require('./utilities');
@@ -23,17 +27,59 @@ app.get('/', (req, res) => {
 });
 
 // Create Account
-app.post('/api/create-account', async (req, res) => {
+app.post('/create-account', async (req, res) => {
     const { fullName, email, password } = req.body;
-    const User = require('./models/user.model');
+
+    if (!fullName){
+        return res
+            .status(400)
+            .json({ error: 'Full name is required' });
+    }
+    if (!email) {
+        return res
+            .status(400)
+            .json({ error: 'Email is required' });
+    }
+    if (!password) {
+        return res
+            .status(400)
+            .json({ error: 'Password is required' });
+    }
+
+    const isUser = await User.findOne({ email: email });
+
+    if (isUser) {
+        return res.json({
+            error: true,
+            message: 'User already exists with this email',
+        });
+    }
+
+    const user = new User({
+        fullName,
+        email,
+        password,
+    });
 
     try {
-        const user = new User({ fullName, email, password });
         await user.save();
-        res.status(201).send({ message: 'User created successfully' });
+        res.status(201).json({ message: 'User created successfully' });
     } catch (error) {
-        res.status(400).send({ error: 'Error creating user' });
+        res.status(500).json({ error: 'Internal server error' });
     }
+
+    const accessToken = jwt.sign(
+        { user },
+        process.env.ACCESS_TOKEN_SECRET,
+        { expiresIn: '36000m' }
+    );
+
+    return res.json({
+        error: false,
+        user,
+        accessToken,
+        message: 'User created successfully',
+    });
 });
 
 app.listen(8000);
